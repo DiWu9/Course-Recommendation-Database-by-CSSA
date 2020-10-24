97903e070a311bf174f110cb26c0b9896e0cd933
ZhongQiyu <zhongq@union.edu>
2020-10-24 15:10:04 -0400

    update 102420

--- cssa.sql
+++ cssa.sql
@@ -1,1 +1,18 @@
-CREATE DATABASE courses; # consider each course to be an object, implement features
+-- Chapter 14
+-- 14.1, 14.2: set up db of courses; need to determine features
+-- 14.3: set up data table of each feature on a course
+-- 14.4: insert data into the table(s)
+CREATE DATABASE courses{
+    course_prefix: CHAR;
+    course_number: NUMBER(500);
+    professor: CHAR;
+}; -- consider each course to be an object, implement features
+CREATE TABLE course_prefix{
+    INSERT
+}
+CREATE TABLE course_number{
+
+}
+CREATE TABLE professor{
+
+}
