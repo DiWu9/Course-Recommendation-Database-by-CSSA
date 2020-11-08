323e951aad8aa58fd4d4d6c1414ebafdcb4654cd
ZhongQiyu <zhongq@union.edu>
2020-11-08 00:01:08 -0500

    update 110820

--- cssa.sql
+++ cssa.sql
@@ -2,6 +2,7 @@
 -- 14.1, 14.2: set up db of courses; need to determine features
 -- 14.3: set up data table of each feature on a course
 -- 14.4: insert data into the table(s)
+-- 14.5: select data from the table(s); REGEXP; patterning; index
 CREATE DATABASE courses{
     course_prefix: CHAR;
     course_number: NUMBER(500);
