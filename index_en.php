67c0c06dd3a4b1d222fbd9d94fa1c0f2adeaf463
ZhongQiyu <zhongq@union.edu>
2020-12-08 02:16:01 -0500

    update 120820

--- index_en.php
+++ index_en.php
@@ -2,5 +2,6 @@
     <body>
         Course Prefix: <?php echo $_POST["course_prefix_input"]; ?>
         Course Number: <?php echo $_POST["course_number_input"]; ?>
+        Professor: <?php echo $_POST["professor_input"]; ?>
     </body>
 </html>
