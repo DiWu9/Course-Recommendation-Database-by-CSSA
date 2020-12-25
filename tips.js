1fd411dfefe7fdc2887079a5039ec456e86521c1
ZhongQiyu <zhongq@union.edu>
2020-12-25 00:48:42 -0500

    initialize 122520

---
+++ tips.js
@@ -0,0 +1,19 @@
+function trimLineBreak(text) {
+    let currentPos = 0;
+    let textLength = text.length;
+    while(currentPos < textLength) {
+        currentPos = text.find("\r");
+
+    }
+}
+
+
+let theThirdTip = document.getElementById("tip_three");
+let theFourthTip = document.getElementById("tip_four");
+document.write(theThirdTip);
+document.write(theFourthTip);
+// let listing = document.getElementById("course_listing");
+// let directory = document.getElementById("directory");
+document.getElementById("tip_four").innerHTML = "";
+// let theFourthTipText = theFourthTip.innerHTML;
+// document.write(theFourthTipText.find("\n"));
